8f5dc446467ec1ba7ea1090c6d63b668e80a8ebe
margav <mikopos@gmail.com>
2025-10-28 20:21:08 +0200

    remove eureka remaining dependencies

--- services/api-gateway/src/main/java/com/flagship/gateway/ApiGatewayApplication.java
+++ services/api-gateway/src/main/java/com/flagship/gateway/ApiGatewayApplication.java
@@ -2,8 +2,6 @@ package com.flagship.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
 /**
  * API Gateway Application
  * <p>
@@ -15,7 +13,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @version 1.0.0
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
   public static void main(String[] args) {
--- services/inventory-service/src/main/java/com/flagship/inventory/InventoryServiceApplication.java
+++ services/inventory-service/src/main/java/com/flagship/inventory/InventoryServiceApplication.java
@@ -3,7 +3,6 @@ package com.flagship.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.EnableKafka;
 
 /**
@@ -18,7 +17,6 @@ import org.springframework.kafka.annotation.EnableKafka;
  * @version 1.0.0
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableKafka
 @EnableCaching
 public class InventoryServiceApplication {
